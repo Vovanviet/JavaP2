823a065ae9fcd7afe11108e9614b89e79c22f21b
Võ Văn Việt <88840717+Vovanviet@users.noreply.github.com>
2022-04-16 08:13:35 +0700

    error + bug

--- src/com/studentmanagement/IO/WriteData.java
+++ src/com/studentmanagement/IO/WriteData.java
@@ -1,25 +1,40 @@
 package com.studentmanagement.IO;
 
+import com.google.gson.Gson;
 import com.studentmanagement.entity.Student;
-import com.studentmanagement.services.StudentServicesImpl;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
+
 import java.util.List;
 
 public class WriteData {
-    public static List<Student>WriteData(List<Student> students){
+    public static List<Student>WriteData(List<Student> students) {
         try {
-            FileWriter myWrite=new FileWriter("Student.txt");
+            FileWriter myWrite=new FileWriter("Student.json");
                 myWrite.write(String.valueOf(students));
                 myWrite.close();
             System.out.println("Successfully wrote to the file");
+//
+//            StringBuilder str = new StringBuilder();
+//            FileOutputStream outputStream = new FileOutputStream("Student.json");
+//            for(Student student: students){
+//                str.append(student.toString());
+//                str.toString().split(",");
+//            }
+//
+//            byte[] strToBytes = str.toString().getBytes();
+//            outputStream.write(strToBytes);
+//
+//            outputStream.close();
+//            System.out.println("Successfully wrote to the file");
+
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        return null;
+
+
+        return students;
     }
 
 
--- src/com/studentmanagement/services/StudentServicesImpl.java
+++ src/com/studentmanagement/services/StudentServicesImpl.java
@@ -1,5 +1,6 @@
 package com.studentmanagement.services;
 
+import com.studentmanagement.IO.GetData;
 import com.studentmanagement.entity.Student;
 
 import java.util.ArrayList;
@@ -7,27 +8,28 @@ import java.util.List;
 import java.util.Scanner;
 
 public class StudentServicesImpl implements StudentServices{
-    List<Student> students =new ArrayList<>();
+    GetData getData=new GetData();
+
+    List<Student> students=getData.getDataTxt();
 
     Scanner scanner=new Scanner(System.in);
     @Override
     public List<Student> getAllStudent() {
         try {
-            if(students.isEmpty()) System.out.println("List is empty");
-            else
-            students.forEach(t->{
-                System.out.println(t);
-            });
+            if (students.isEmpty()){
+                System.out.println("List is empty");
+            }else {
+            return students;}
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+        return students;
     }
     @Override
     public void updateStudent(String name) {
         try {
-                for (int i = 0; i < students.size(); i++) {
-                    if (name.equals(students.get(i).getName())) {
+            students.forEach(t->{
+                if (name.equals(t)){
                         System.out.println("Enter new RollNo:");
                         String newRollNo = scanner.next();
                         System.out.println("Enter Name or New Name:");
@@ -36,14 +38,30 @@ public class StudentServicesImpl implements StudentServices{
                         int newAge = scanner.nextInt();
                         System.out.println("Enter Mark:");
                         Double mark = scanner.nextDouble();
-
-                        students.get(i).setRollNo(newRollNo);
-                        students.get(i).setName(newName);
-                        students.get(i).setAge(newAge);
-                        students.get(i).setMark(mark);
-                        System.out.println(students.get(i));
-                    }
+                        t.setRollNo(newRollNo);
+                        t.setName(newName);
+                        t.setAge(newAge);
+                        t.setMark(mark);
                 }
+            });
+////                for (int i = 0; i < students.size(); i++) {
+////                    if (name.equals(students.get(i).getName())) {
+////                        System.out.println("Enter new RollNo:");
+////                        String newRollNo = scanner.next();
+////                        System.out.println("Enter Name or New Name:");
+////                        String newName = scanner.next();
+////                        System.out.println("Enter Age:");
+////                        int newAge = scanner.nextInt();
+////                        System.out.println("Enter Mark:");
+////                        Double mark = scanner.nextDouble();
+////
+////                        students.get(i).setRollNo(newRollNo);
+////                        students.get(i).setName(newName);
+////                        students.get(i).setAge(newAge);
+////                        students.get(i).setMark(mark);
+////                        System.out.println(students.get(i));
+//                    }
+//                }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -52,22 +70,22 @@ public class StudentServicesImpl implements StudentServices{
     @Override
     public void deleteStudent(String name) {
         try {
-                for (int i = 0; i < students.size(); i++) {
-                    if (name.equals(students.get(i).getName())) {
-                        System.out.println("Are you sure?\n1.Yes\n2.No");
-                        int choose = scanner.nextInt();
-                        switch (choose) {
-                            case 1:
-                                students.remove(name);
-                                System.out.println("Deleted successfully!");
-                                break;
-                            case 2:
-                                System.out.println("The operation has been cancelled!!!");
-                            default:
-                                System.out.println("Number input invalid!");
-                        }
-                    }
-                }
+//                for (int i = 0; i < students.size(); i++) {
+//                    if (name.equals(students.get(i).getName())) {
+//                        System.out.println("Are you sure?\n1.Yes\n2.No");
+//                        int choose = scanner.nextInt();
+//                        switch (choose) {
+//                            case 1:
+//                                students.remove(name);
+//                                System.out.println("Deleted successfully!");
+//                                break;
+//                            case 2:
+//                                System.out.println("The operation has been cancelled!!!");
+//                            default:
+//                                System.out.println("Number input invalid!");
+//                        }
+//                    }
+//                }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -76,16 +94,9 @@ public class StudentServicesImpl implements StudentServices{
     @Override
     public void addStudent(String rollNo,String name,int age,double mark) {
         try {
-                for (int i = 0; i < students.size(); i++) {
-                    if (name.equals(students.get(i).getName())) {
-                        System.out.println("Student already exist!");
-                    } else {
 
-                        students.add(new Student(rollNo, name, age, mark));
-                        System.out.println(students.get(students.size() - 1));
-                    }
-                }
 
+            System.out.println(students.get(students.size() - 1));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -94,15 +105,13 @@ public class StudentServicesImpl implements StudentServices{
     @Override
     public void searchStudent(String name) {
         try {
-            if (students.size()==0){
-                System.out.println("List is empty!");
-            }else {
+            students=getData.getDataTxt();
                 for (int i = 0; i < students.size(); i++) {
                     if (name.equals(students.get(i).getName())) {
                         System.out.println(students.get(i));
                     }
                 }
-            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
